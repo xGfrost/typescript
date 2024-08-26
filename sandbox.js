bd0335267450188cab0c7c55a40ccdedce5cebfa
xGfrost <fadelakbar528@gmail.com>
2024-08-26 22:35:30 +0700

    push 3

--- sandbox.js
+++ sandbox.js
@@ -5,16 +5,40 @@
 // inputs.forEach(input =>{
 //     console.log(input)
 // })
-var character = 'mario';
-var age = 30;
-var isBlackBelt = false;
-// character = 20;
-character = 'luigi';
-// age = 'yoshi';
-age = 40;
-// isBlackBelt = 'yes';
-isBlackBelt = true;
-var circ = function (diameter) {
-    return diameter * Math.PI;
+// let character = 'mario';
+// let age = 30;
+// let isBlackBelt = false;
+// // character = 20;
+// character = 'luigi';
+// // age = 'yoshi';
+// age = 40;
+// // isBlackBelt = 'yes';
+// isBlackBelt = true;
+// const circ = (diameter: number) => {
+//     return diameter * Math.PI
+// }
+// console.log(circ(7.5));
+var names = ['luigi', 'mario', 'yoshi'];
+names.push('toad');
+// names.push();
+// names[0] = 3;
+var numbers = [10, 20, 30, 40];
+numbers.push(25);
+var mixed = ['ken', 4, 'chun-li', 8, 9];
+mixed.push('ruy');
+mixed.push(10);
+mixed[0] = 3;
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30,
 };
-console.log(circ(7.5));
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = 21
+// ninja.skills = ['fighting', 'sneaking']
+ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    age: 21,
+};
--- sandbox.ts
+++ sandbox.ts
@@ -10,21 +10,55 @@
 //     console.log(input)
 // })
 
-let character = 'mario';
-let age = 30;
-let isBlackBelt = false;
+// let character = 'mario';
+// let age = 30;
+// let isBlackBelt = false;
 
-// character = 20;
-character = 'luigi';
+// // character = 20;
+// character = 'luigi';
 
-// age = 'yoshi';
-age = 40;
+// // age = 'yoshi';
+// age = 40;
 
-// isBlackBelt = 'yes';
-isBlackBelt = true;
+// // isBlackBelt = 'yes';
+// isBlackBelt = true;
 
-const circ = (diameter: number) => {
-    return diameter * Math.PI
+// const circ = (diameter: number) => {
+//     return diameter * Math.PI
+// }
+
+// console.log(circ(7.5));
+
+let names = ['luigi', 'mario', 'yoshi'];
+
+names.push('toad');
+// names.push();
+// names[0] = 3;
+
+let numbers = [10, 20, 30, 40];
+
+numbers.push(25);
+
+let mixed = ['ken', 4, 'chun-li', 8, 9];
+
+mixed.push('ruy');
+mixed.push(10);
+mixed[0] = 3;
+
+
+let ninja = {
+    name: 'mario',
+    belt: 'black',
+    age : 30,
+};
+
+ninja.age = 40;
+ninja.name = 'ryu'
+// ninja.age = 21
+// ninja.skills = ['fighting', 'sneaking']
+
+ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    age : 21,
 }
-
-console.log(circ(7.5));
