2d0c73dc6c3b6b93b192e787c67400d2e1e3b369
xGfrost <fadelakbar528@gmail.com>
2024-08-26 22:25:00 +0700

    push 2

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,20 @@
-var character = 'luigi';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
+// const character = 'luigi'
+// console.log(character);
+// const inputs = document.querySelectorAll('input');
+// console.log(inputs);
+// inputs.forEach(input =>{
+//     console.log(input)
+// })
+var character = 'mario';
+var age = 30;
+var isBlackBelt = false;
+// character = 20;
+character = 'luigi';
+// age = 'yoshi';
+age = 40;
+// isBlackBelt = 'yes';
+isBlackBelt = true;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7.5));
--- sandbox.ts
+++ sandbox.ts
@@ -1,11 +1,30 @@
-const character = 'luigi'
+// const character = 'luigi'
 
-console.log(character);
+// console.log(character);
 
-const inputs = document.querySelectorAll('input');
+// const inputs = document.querySelectorAll('input');
 
-console.log(inputs);
+// console.log(inputs);
 
-inputs.forEach(input =>{
-    console.log(input)
-})
+// inputs.forEach(input =>{
+//     console.log(input)
+// })
+
+let character = 'mario';
+let age = 30;
+let isBlackBelt = false;
+
+// character = 20;
+character = 'luigi';
+
+// age = 'yoshi';
+age = 40;
+
+// isBlackBelt = 'yes';
+isBlackBelt = true;
+
+const circ = (diameter: number) => {
+    return diameter * Math.PI
+}
+
+console.log(circ(7.5));
