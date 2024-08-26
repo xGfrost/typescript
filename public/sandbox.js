0708e7b9406dae2ad80bd8e4e21292eb70b5efff
xGfrost <fadelakbar528@gmail.com>
2024-08-26 23:03:10 +0700

    push 6

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,3 +1,4 @@
+"use strict";
 // const character = 'luigi'
 // console.log(character);
 // const inputs = document.querySelectorAll('input');
@@ -66,20 +67,21 @@
 //     beltColor: string,
 // };
 // frosttwo = { name: 'mario', age: 20, beltColor: 'black'}
-var age = 25;
-age = true;
-console.log(age);
-age = 'hello';
-console.log(age);
-age = { name: 'luigi' };
-console.log(age);
-var mixed = [];
-mixed.push(5);
-mixed.push('mario');
-mixed.push(false);
-console.log(mixed);
-var ninja;
-ninja = { name: 'yoshi', age: 25 };
-console.log(ninja);
-ninja = { name: 25, age: 'yoshi' };
-console.log(ninja);
+// let age: any = 25;
+// age = true;
+// console.log(age)
+// age= 'hello'
+// console.log(age)
+// age = { name: 'luigi'}
+// console.log(age)
+// let mixed: any[] = [];
+// mixed.push(5);
+// mixed.push('mario');
+// mixed.push(false);
+// console.log(mixed);
+// let ninja: { name: any, age: any};
+// ninja = {name: 'yoshi', age: 25};
+// console.log(ninja);
+// ninja = {name: 25, age: 'yoshi'};
+// console.log(ninja);
+console.log('test');
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -100,24 +100,26 @@
 
 // frosttwo = { name: 'mario', age: 20, beltColor: 'black'}
 
-let age: any = 25;
-age = true;
-console.log(age)
-age= 'hello'
-console.log(age)
-age = { name: 'luigi'}
-console.log(age)
+// let age: any = 25;
+// age = true;
+// console.log(age)
+// age= 'hello'
+// console.log(age)
+// age = { name: 'luigi'}
+// console.log(age)
 
-let mixed: any[] = [];
+// let mixed: any[] = [];
 
-mixed.push(5);
-mixed.push('mario');
-mixed.push(false);
-console.log(mixed);
+// mixed.push(5);
+// mixed.push('mario');
+// mixed.push(false);
+// console.log(mixed);
 
-let ninja: { name: any, age: any};
+// let ninja: { name: any, age: any};
 
-ninja = {name: 'yoshi', age: 25};
-console.log(ninja);
-ninja = {name: 25, age: 'yoshi'};
-console.log(ninja);
+// ninja = {name: 'yoshi', age: 25};
+// console.log(ninja);
+// ninja = {name: 25, age: 'yoshi'};
+// console.log(ninja);
+
+console.log('test')
