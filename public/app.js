1200764014548f83fd3e1479a4733c997ff888aa
xGfrost <fadelakbar528@gmail.com>
2024-08-27 16:02:34 +0700

    push 11

--- public/app.js
+++ public/app.js
@@ -3,9 +3,30 @@
 // if (anchor) {
 //     console.log(anchor.href);
 // }
+//classes
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c,
+            this.details = d,
+            this.amount = a;
+    }
+    format() {
+        return `${this.client} owes RP${this.amount} for ${this.details}`;
+    }
+}
+const inOne = new Invoice('mario', 'work on the mario website', 250);
+const intwo = new Invoice('luigi', 'work on the luigi website', 300);
+let Invoices = [];
+Invoices.push(inOne);
+Invoices.push(intwo);
+console.log(Invoices);
+inOne.client = 'yoshi';
+intwo.amount = 400;
+console.log(inOne, intwo);
 // const form = document.querySelector('form')!;
 const form = document.querySelector('.new-item-form');
 // console.log(form.children);
+//inputs
 const type = document.querySelector('#type');
 const toFrom = document.querySelector('#toFrom');
 const details = document.querySelector('#details');
--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,44 @@
 //     console.log(anchor.href);
 // }
 
+//classes
+class Invoice{
+    client: string;
+    details: string;
+    amount: number;
+
+    constructor(c: string, d:string, a: number){
+        this.client = c,
+        this.details = d,
+        this.amount = a;
+    }
+
+    format() {
+        return `${this.client} owes RP${this.amount} for ${this.details}`;
+    }
+}
+
+const inOne = new Invoice('mario', 'work on the mario website', 250);
+const intwo = new Invoice('luigi', 'work on the luigi website', 300);
+
+let Invoices: Invoice[] = [];
+Invoices.push(inOne);
+Invoices.push(intwo);
+
+console.log(Invoices);
+
+inOne.client = 'yoshi';
+intwo.amount = 400;
+
+console.log(inOne, intwo)
+
+
+
 // const form = document.querySelector('form')!;
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 // console.log(form.children);
 
+//inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
 const toFrom = document.querySelector('#toFrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
